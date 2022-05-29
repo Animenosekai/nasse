081af0fc9bf73e192e6c58711cc214c441f44ce6
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2022-05-29 20:16:28 +0200

    [update] only log exceptions when needed

--- nasse/exceptions/base.py
+++ nasse/exceptions/base.py
@@ -6,9 +6,11 @@ class NasseException(Exception):
     STATUS_CODE = 500
     MESSAGE = "An unexpected error occured on the server"
     EXCEPTION_NAME = "SERVER_ERROR"
+    LOG = True
 
     def __init__(self, message: str = None, *args: object) -> None:
         if message is not None:
             self.MESSAGE = str(message)
         super().__init__(self.MESSAGE, *args)
-        log(self.MESSAGE, level=LogLevels.ERROR)
+        if self.LOG:
+            log(self.MESSAGE, level=LogLevels.ERROR)
